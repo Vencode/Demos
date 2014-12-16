f6de97abf400414bac1733ade85aaf5eed8b01c7
Vencode <ivo.ventura88@gmail.com>
2014-12-16 17:30:45 +0000

    Areas Implementation

---
+++ Demo_SimpleBlog/Demo_SimpleBlog/Controllers/PostsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Demo_SimpleBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return Content("Hello World");
+        }
+    }
+}

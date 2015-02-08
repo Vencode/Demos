decbc4514bb6f06bf8017916777fe26e5e162f79
vencode <ivo.ventura88@gmail.com>
2015-02-08 12:12:11 +0000

    Added Bundles of JS and CSS

--- Demo_SimpleBlog/Demo_SimpleBlog/Global.asax.cs
+++ Demo_SimpleBlog/Demo_SimpleBlog/Global.asax.cs
@@ -1,5 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using Demo_SimpleBlog.App_Start;
+using WebGrease.Configuration;
 
 namespace Demo_SimpleBlog
 {
@@ -8,7 +12,13 @@ namespace Demo_SimpleBlog
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configuration.EnsureInitialized(); 
         }
     }
 }

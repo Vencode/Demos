985908f651a2e45a554f0c309dbd473ea1010320
vencode <ivo.ventura88@gmail.com>
2015-02-08 22:32:47 +0000

    Created the Admin Layout

--- Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,14 +1,16 @@
 ﻿using System.Web.Mvc;
+using Demo_SimpleBlog.Infrastructure;
 
 namespace Demo_SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
         // GET: Admin/Posts
         public ActionResult Index()
         {
-            return Content("ADMIN's Posts");
+            return View();
         }
     }
 }
--- Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,14 +1,16 @@
 ﻿using System.Web.Mvc;
+using Demo_SimpleBlog.Infrastructure;
 
 namespace Demo_SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return Content("USERS!");
+            return View();
         }
     }
 }

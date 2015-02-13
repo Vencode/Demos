369586bbce21ff14704a21d7034ff48574c84ff1
vencode <ivo.ventura88@gmail.com>
2015-02-13 23:43:41 +0000

    Added Pagination and Index action on Posts Admin

--- Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,15 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Demo_SimpleBlog.Areas.Admin.ViewModels;
 using Demo_SimpleBlog.Infrastructure;
+using Demo_SimpleBlog.Models;
+using NHibernate.Linq;
 
 namespace Demo_SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin"), SelectedTab("posts")]
     public class PostsController : Controller
     {
+        private const int PostsPerPage = 5;
+
         // GET: Admin/Posts
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var totalPostCount = Database.Session.Query<Post>().Count();
+
+            var currentPostPage = Database.Session.Query<Post>()
+                .OrderByDescending(pos => pos.CreatedAt)
+                .Skip((page - 1)*PostsPerPage)
+                .Take(PostsPerPage)
+                .ToList();
+
+            return View(new PostsIndex
+            {
+                Posts = new PageData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
+            });
         }
     }
 }
---
+++ Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Demo_SimpleBlog.Infrastructure;
+using Demo_SimpleBlog.Models;
+
+namespace Demo_SimpleBlog.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PageData<Post> Posts { get; set; }
+    }
+}

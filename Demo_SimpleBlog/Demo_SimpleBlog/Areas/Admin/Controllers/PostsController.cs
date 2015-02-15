9bf9549196f473a515f42b9f37be61b56fd35a49
vencode <ivo.ventura88@gmail.com>
2015-02-15 12:09:21 +0000

    Update/Create new posts

--- Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ Demo_SimpleBlog/Demo_SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Demo_SimpleBlog.Areas.Admin.ViewModels;
 using Demo_SimpleBlog.Infrastructure;
@@ -26,6 +27,68 @@ namespace Demo_SimpleBlog.Areas.Admin.Controllers
             return View(new PostsIndex
             {
                 Posts = new PageData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
+            });
+        }
+
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId != null;
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            Post post;
+            if (form.IsNew)
+            {
+                post = new Post
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    User = Auth.User
+                };
+            }
+            else
+            {
+                post = Database.Session.Load<Post>(form.PostId);
+
+                if (post == null)
+                    return HttpNotFound();
+
+                post.UpdatedAt = DateTime.UtcNow;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+
+            if (post == null)
+                return HttpNotFound();
+
+            return View("Form", new PostsForm
+            {
+                IsNew   = false,
+                PostId  = id,
+                Content = post.Content,
+                Slug    = post.Slug,
+                Title   = post.Title 
             });
         }
     }
